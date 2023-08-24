bc529137b79a21760f37901f72584d239e693068
kambereBr <brunokambere@gmail.com>
2023-08-24 02:16:26 +0200

    import form and BookList components intoHome

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Form from './Form';
 import BookList from './BookList';
 
-const Home = () => {
-  const [books, setBooks] = useState([]);
-
-  const getId = () => Math.floor(Math.random() * 99999);
-
-  const handleSubmitBook = (title, description, author) => {
-    const book = {
-      id: getId(), title, author, description,
-    };
-    setBooks([...books, book]);
-  };
-
-  const handleDeleteBook = (id) => {
-    const listBook = books.filter((book) => book.id !== id);
-    setBooks(listBook);
-  };
-
-  return (
-    <div>
-      <Form addBook={handleSubmitBook} />
-      <BookList books={books} onDelete={handleDeleteBook} />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <Form />
+    <BookList />
+  </div>
+);
 
 export default Home;

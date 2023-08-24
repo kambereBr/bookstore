812f41d7a7eaafd1e5dc5e6662a52c77f35d693d
kambereBr <brunokambere@gmail.com>
2023-08-24 02:09:12 +0200

    dispatch changes in Categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <div>All categories ...</div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const { categories } = useSelector((store) => store.categories);
+  useEffect(() => {
+    dispatch(checkStatus());
+  }, [categories]);
+  return (
+    <div>{categories}</div>
+  );
+};
 
 export default Categories;

6e56fa3de7e54eec0aaa48cde25d85cb3a91f335
kambereBr <brunokambere@gmail.com>
2023-08-26 13:14:26 +0200

    refactor form component for better styling

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Button from './Button';
 import { addNewBook } from '../../redux/books/booksSlice';
+import styles from '../../styles/Form.module.css';
 
 const Form = () => {
   const [title, setTitle] = useState('');
@@ -25,15 +26,32 @@ const Form = () => {
     setCategory('');
   };
 
+  const myStyle = {
+    cursor: 'pointer',
+    padding: 12,
+    borderRadius: 3,
+    width: 150,
+    height: 45,
+    backgroundColor: '#0290ff',
+    border: 'none',
+    letterSpacing: 0.5,
+    color: '#fff',
+    fontSize: 13,
+    fontWeight: 700,
+    fontDamily: 'Roboto Slab',
+  };
+
   return (
-    <div>
-      <h1>Add a new Book</h1>
+    <div className={styles.formContainer}>
+      <hr />
+      <h1>ADD NEW BOOK</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Enter the book title"
+          className={styles.inputTitle}
           required
         />
         <input
@@ -41,15 +59,18 @@ const Form = () => {
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
           placeholder="Enter the author name"
+          className={styles.inputTitle}
           required
         />
-        <textarea
+        <input
+          type="text"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
           placeholder="Enter the book category"
+          className={styles.inputTitle}
           required
         />
-        <Button className="submit-button" event={() => null} label="Add new Book" />
+        <Button className="submit-button" style={myStyle} event={() => null} label="ADD BOOK" />
       </form>
     </div>
   );

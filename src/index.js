b75653f839741fda3d578020408cae921a4c09ac
kambereBr <brunokambere@gmail.com>
2023-08-26 13:12:03 +0200

    refactor importing style path

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

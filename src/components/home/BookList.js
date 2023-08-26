c7c908d533d79bbc2729d8070da6e464146cbc68
kambereBr <brunokambere@gmail.com>
2023-08-26 13:14:40 +0200

    enhance the book list

--- src/components/home/BookList.js
+++ src/components/home/BookList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import { fetchBooks } from '../../redux/books/booksSlice';
+import styles from '../../styles/Book.module.css';
 
 const BookList = () => {
   const dispatch = useDispatch();
@@ -12,15 +13,17 @@ const BookList = () => {
   const { books } = useSelector((store) => store.books);
   return (
     <div>
-      {Object.keys(books).map((key) => (
-        <Book
-          key={key}
-          id={key}
-          title={books[key][0].title}
-          category={books[key][0].category}
-          author={books[key][0].author}
-        />
-      ))}
+      <ul className={styles.bookList}>
+        {Object.keys(books).map((key) => (
+          <Book
+            key={key}
+            id={key}
+            title={books[key][0].title}
+            category={books[key][0].category}
+            author={books[key][0].author}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
